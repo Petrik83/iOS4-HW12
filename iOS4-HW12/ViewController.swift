84cf04c98f99ddef7f84bf277fc513998bf24430
Aliaksandr Piatrovich <alex.petrovich83@gmail.com>
2021-12-04 09:06:10 +0300

    Initial Commit

---
+++ iOS4-HW12/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  iOS4-HW12
+//
+//  Created by Александр Петрович on 04.12.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
